16445735548bdea25b1f3360a6595c7114f225ed
babychoomp <babychoomp@gmail.com>
2023-04-18 11:37:07 +0900

    create WinterSoldier Spring project

---
+++ src/main/java/com/dami/wintersoldier/WinterSoldierApplication.java
@@ -0,0 +1,13 @@
+package com.dami.wintersoldier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterSoldierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterSoldierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dami/wintersoldier/WinterSoldierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dami.wintersoldier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterSoldierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
